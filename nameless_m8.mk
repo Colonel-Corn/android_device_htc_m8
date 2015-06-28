eb0a65c17034a4f3ff08dc81a09e54c01654173d
fizbanrapper <jeremiah.akers@outlook.com>
2015-06-27 20:38:33 -0400

    nameless
    Change-Id: I6c1233daeef8e1dccf036ebe4a0cc09a5269098e

--- nameless_m8.mk
+++ nameless_m8.mk
@@ -4,6 +4,8 @@ $(call inherit-product, device/htc/m8/full_m8.mk)
 $(call inherit-product, vendor/nameless/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_NAME := cm_m8
+include vendor/nameless/config/common.mk
+$(call inherit-product, vendor/nameless/config/apns.mk)
+PRODUCT_NAME := nameless_m8
